dae9e4479c79e8b78aeafe95c78a2520f29f0082
ndonaghy12id40226743 <46711366+ndonaghy12id40226743@users.noreply.github.com>
2019-11-06 11:44:49 +0000

    Update querydb37.sql

--- querydb37.sql
+++ querydb37.sql
@@ -1,1 +1,7 @@
-SELECT alt.LeaseID, alt.ApartmentNo,Building.BuildingNameorNum,Building.Street,Building.PostCode,(alt.NumBedrooms-alt.NumTenants) AS 'NumFreeBedrooms' FROM (SELECT Apartment.ApartmentNo,Apartment.NumBedrooms,Apartment.BuildingID,lt.LeaseID,lt.NumTenants FROM Apartment INNER JOIN (SELECT LeaseTenants.LeaseID,LeaseTenants.TenantID, Lease.ApartmentID, COUNT(*) AS 'NumTenants' FROM LeaseTenants INNER JOIN Lease ON LeaseTenants.LeaseID = Lease.LeaseID WHERE (SUBDATE(CURRENT_DATE, INTERVAL Lease.Duration MONTH) < Lease.StartDate) GROUP BY LeaseTenants.LeaseID)lt ON lt.ApartmentID = Apartment.ApartmentID)alt INNER JOIN Building ON alt.BuildingID = Building.BuildingID WHERE (alt.NumBedrooms - alt.NumTenants >0);
+SELECT alt.LeaseID, alt.ApartmentNo,Building.BuildingNameorNum,Building.Street,Building.PostCode,(alt.NumBedrooms-alt.NumTenants) AS 'NumFreeBedrooms' 
+FROM (SELECT Apartment.ApartmentNo,Apartment.NumBedrooms,Apartment.BuildingID,lt.LeaseID,lt.NumTenants 
+FROM Apartment 
+INNER JOIN (SELECT LeaseTenants.LeaseID,LeaseTenants.TenantID, Lease.ApartmentID, COUNT(*) AS 'NumTenants' 
+FROM LeaseTenants 
+INNER JOIN Lease ON LeaseTenants.LeaseID = Lease.LeaseID WHERE (SUBDATE(CURRENT_DATE, INTERVAL Lease.Duration MONTH) < Lease.StartDate) GROUP BY LeaseTenants.LeaseID)lt ON lt.ApartmentID = Apartment.ApartmentID)alt 
+INNER JOIN Building ON alt.BuildingID = Building.BuildingID WHERE (alt.NumBedrooms - alt.NumTenants >0);
